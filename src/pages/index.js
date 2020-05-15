becfca4bb47deb259bdc34a9d10c8a2e22a77e64
Finn Thye <piqueen@gmail.com>
2020-05-15 10:47:20 -0600

    Add about page, component folder and Header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Cool Returnship</div>
+export default function Home() {
+  return ( 
+    <div style={{ color: `purple` }}>
+      <h1 style={{ color: `purple`, fontSize: `72px` }}>Hello Self!</h1>
+      <p> Really life is quite wonderful whichever way you go.</p>
+      <img src="https://i.pinimg.com/236x/97/5f/79/975f79089e9ebfa65837bce4a9b8afd2.jpg" alt="" />
+    </div>
+  )
+}
